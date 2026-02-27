9e469883e2e54aa1c0ba1fc630e975b6eb86479c
CWIR3 <humumshaikh0@gmail.com>
2026-02-28 00:03:03 +0530

    Update dht11_main.cpp

--- Projects/DHT11/dht11_main.cpp
+++ Projects/DHT11/dht11_main.cpp
@@ -1,4 +1,5 @@
 //CWIR3
+//Written for ESP32 DOIT DEVKIT V1
 
 //This is the program to configure the DHT 11 temperature and humidity sensor with the ESP32 //
 #include <DHT.h> //This is the header file for the DHT and you need to install the libraries seperately//
