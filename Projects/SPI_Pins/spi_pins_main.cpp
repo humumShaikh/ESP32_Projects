095747d49ec862e784c76e249afcacbfb3bb8798
CWIR3 <humumshaikh0@gmail.com>
2026-03-01 22:46:18 +0530

    Create spi_pins_main.cpp

---
+++ Projects/SPI_Pins/spi_pins_main.cpp
@@ -0,0 +1,20 @@
+void setup(){
+
+  Serial.begin(115200);
+
+  Serial.print("MOSI : "); //usually 23//
+  Serial.println(MOSI);
+  
+  Serial.print("MISO : "); //usually 19//
+  Serial.println(MISO);
+
+  Serial.print("SCK : "); //usually 18//
+  Serial.println(SCK);
+
+  Serial.print("SS : "); //usually 5//
+  Serial.println(SS);
+}
+
+void loop(){
+  
+}
