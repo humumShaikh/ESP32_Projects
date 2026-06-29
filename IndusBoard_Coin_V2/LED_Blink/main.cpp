2098750a766906a0bcbe6c90358ca56f572cdb47
CWIR3 <humumshaikh0@gmail.com>
2026-06-29 16:04:40 +0530

    Add files via upload

---
+++ IndusBoard_Coin_V2/LED_Blink/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(33 , OUTPUT);
+  delay(2000);
+  Serial.println("Programmed Successfully \n");
+}
+
+void loop()
+{
+  digitalWrite(33 , HIGH);
+  Serial.println("ON \n");
+  delay(100);
+  digitalWrite(33 , LOW);
+  Serial.println("OFF \n");
+  delay(100);
+}
