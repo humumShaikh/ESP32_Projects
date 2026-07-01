c5a214e074b7b04e69bda284fdf7cf96be0dbd6f
CWIR3 <humumshaikh0@gmail.com>
2026-07-01 09:34:09 +0530

    Update main.cpp

--- IndusBoard_Coin_V2/Touch_Keypad/main.cpp
+++ IndusBoard_Coin_V2/Touch_Keypad/main.cpp
@@ -1,38 +1,31 @@
 #include <Arduino.h>
-#include<Adafruit_SSD1306.h>
-
-#define OLED_RESET 4
-#define OLED_ADDRESS 0x3C
-Adafruit_SSD1306 OLED(OLED_RESET);
-
-
 
 
 const int num_keys = 10;
 int touch_pins[num_keys] = { 1, 3, 4, 7, 6, 5, 10, 11, 12,
 13 };
 
-int baseline;
+// int baseline;
 
-void calibrate_baselines() {
- // Optional: Calibrate and print baselines (can be removed
-// if not needed)
+// void calibrate_baselines() {
+//  // Optional: Calibrate and print baselines (can be removed
+// // if not needed)
 
- for (int i = 0; i < num_keys; i++) {
- uint32_t sum = 0;
- // Average 100 readings for stable baseline
- for (int j = 0; j < 100; j++) {
- sum += touchRead(touch_pins[i]);
- delay(2); // Short delay between reads
- }
- baseline = sum / 100;
- Serial.print("Baseline for pin ");
- Serial.print(touch_pins[i]);
- Serial.print(": ");
- Serial.println(baseline);
- }
- Serial.println("Calibration complete.");
-}
+//  for (int i = 0; i < num_keys; i++) {
+//  uint32_t sum = 0;
+//  // Average 100 readings for stable baseline
+//  for (int j = 0; j < 100; j++) {
+//  sum += touchRead(touch_pins[i]);
+//  delay(2); // Short delay between reads
+//  }
+//  baseline = sum / 100;
+//  Serial.print("Baseline for pin ");
+//  Serial.print(touch_pins[i]);
+//  Serial.print(": ");
+//  Serial.println(baseline);
+//  }
+//  Serial.println("Calibration complete.");
+// }
 
 
 void setup()
@@ -40,19 +33,13 @@ void setup()
 
   Serial.begin(115200);
 
-  calibrate_baselines(); 
+  // calibrate_baselines(); 
 
 }
 
 
 void loop()
 {
-
-  OLED.setTextSize(2); 
-  OLED.setTextColor(WHITE);
-  OLED.setCursor(0,0); 
-  OLED.println("AI&QT"); 
-  OLED.display();
 
   int value = touchRead(touch_pins[8]);
   Serial.println(value); 
